1e3bfc77a550c39e404b460394429f0c004a0eb2
Anton Kovalenko <akovalenko@anton-hpnew.home.sw4me.com>
2016-06-16 01:32:28 +0300

    Don't redefine container_of (reduce warnings)

--- usr/list.h
+++ usr/list.h
@@ -10,9 +10,11 @@
 #define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
 #endif
 
+#ifndef container_of
 #define container_of(ptr, type, member) ({			\
         const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
         (type *)( (char *)__mptr - offsetof(type,member) );})
+#endif
 
 struct list_head {
 	struct list_head *next, *prev;
